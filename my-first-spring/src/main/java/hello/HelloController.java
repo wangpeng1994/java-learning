6e8871fbb11b3e390351b5ff5bb3c92d6cab16cd
wangpeng1994 <947034046@qq.com>
2020-05-31 23:21:58 +0800

    增加 spring + mybatis 项目演示

---
+++ my-first-spring/src/main/java/hello/HelloController.java
@@ -0,0 +1,20 @@
+package hello;
+
+import hello.dao.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public Object search() {
+        return userMapper.getUserById(1);
+    }
+}
