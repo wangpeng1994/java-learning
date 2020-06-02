666b7926f1bf515ae2a7376839dbcf6198931f7e
Peng <peng@MacBook-Pro.local>
2020-06-02 20:54:45 +0800

    freemaker

--- my-first-spring/src/main/java/hello/HelloController.java
+++ my-first-spring/src/main/java/hello/HelloController.java
@@ -1,10 +1,16 @@
 package hello;
 
+import hello.entity.RankItem;
 import hello.service.RankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -16,5 +22,13 @@ public class HelloController {
     @ResponseBody
     public Object search() {
         return rankService.getRank();
+    }
+
+    @RequestMapping("/rank")
+    public ModelAndView index() {
+        List<RankItem> items = rankService.getRank();
+        Map<String, Object> model = new HashMap<>();
+        model.put("items", items);
+        return new ModelAndView("rank", model);
     }
 }
