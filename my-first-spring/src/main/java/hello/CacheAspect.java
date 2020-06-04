86539e7ad10291538ea6ccaf3665d36780bcb015
wangpeng1994 <947034046@qq.com>
2020-06-05 00:42:11 +0800

    使用 docker + Redis + AOP 实现对方法返回值进行缓存

--- my-first-spring/src/main/java/hello/CacheAspect.java
+++ my-first-spring/src/main/java/hello/CacheAspect.java
@@ -3,14 +3,34 @@ package hello;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Aspect
 @Configuration
 public class CacheAspect {
+    // Map<String, Object> cache = new HashMap<>();
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
     @Around("@annotation(hello.anno.Cache)")
     public Object cache(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("method is called!");
-        return joinPoint.proceed();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String methodName = signature.getName();
+        Object cachedValue = redisTemplate.opsForValue().get(methodName);
+
+        if (cachedValue != null) {
+            System.out.println("Get value from cache!");
+            System.out.println(cachedValue);
+            return cachedValue;
+        } else {
+            System.out.println("Get value from database!");
+            Object realValue = joinPoint.proceed();
+            redisTemplate.opsForValue().set(methodName, realValue);
+            return realValue;
+        }
     }
 }
--- my-first-spring/src/main/java/hello/entity/RankItem.java
+++ my-first-spring/src/main/java/hello/entity/RankItem.java
@@ -1,6 +1,8 @@
 package hello.entity;
 
-public class RankItem {
+import java.io.Serializable;
+
+public class RankItem implements Serializable {
     private int score;
     private User user;
 
